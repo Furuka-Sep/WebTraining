4e7b9225cf2ba5a86e70cc9970064f2222ac67ef
Furuya <Furuka0928@gmail.com>
2021-01-12 14:44:00 +0900

    20210112

---
+++ high&low/WebContent/WEB-INF/view/game.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="model.PlayData" %>
+<%
+PlayData pd=(PlayData)session.getAttribute("pd");
+String[] cards=pd.get
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>High&amp;Low</title>
+</head>
+<body>
+<div id="card">
+<img src="images/"
+</div>
+</body>
+</html>
--- high&low/src/model/GameLogic.java
+++ high&low/src/model/GameLogic.java
@@ -8,15 +8,27 @@ public class GameLogic {
 		int[] draw= {Integer.parseInt(player),Integer.parseInt(dealer)};
 		pd.setCount(pd.getCount()+2);
 		if(judge[0]==judge[pd.getChoise()]) {
+			//Playerの選択がHighの場合
 			if(draw[0]>draw[1]) {
-				win;
+				//勝ち
+				pd.setHighScore(pd.getHighScore()+1);
 			}else if(draw[0]<draw[1]) {
-				lose;
+				//負け
+				//結果を出して最初の画面に戻るボタン
 			}else {
-
+				//同じ値(スコアは増やさずもう一度)
 			}
 		}else if(judge[1]==judge[pd.getChoise()]) {
+			//Playerの選択がLowの場合
+			if(draw[0]<draw[1]) {
+				//勝ち
+				pd.setHighScore(pd.getHighScore()+1);
+			}else if(draw[0]>draw[1]) {
+				//負け
+				//結果を出して最初の画面に戻るボタン
+			}else {
+				//同じ値(スコアは増やさずもう一度)
+			}
 		}
-		pd.setHighScore(pd.getHighScore()+1);
 	}
 }
